115bc0aca394101eb6f664c94f615978c8bf91b9
unknown <davijannsen@gmail.com>
2025-07-12 19:15:48 -0300

    adição das consultas

--- clinic-management-system-/src/main/java/clinicamed/controller/PacienteController.java
+++ clinic-management-system-/src/main/java/clinicamed/controller/PacienteController.java
@@ -1,17 +1,20 @@
 package clinicamed.controller;
 
 import clinicamed.model.Paciente;
-import clinicamed.utils.Navegacao;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PacienteController extends Basecontroller implements Initializable {
+public class PacienteController implements Initializable {
     private Paciente paciente;
     @FXML
     private Label labelNome;
@@ -48,20 +51,28 @@ public class PacienteController extends Basecontroller implements Initializable 
         labelNomeTitle.setText(paciente.getNome());
         labelNome.setText(paciente.getNome());
     }
-    
-    public void handleEditarPerfil() {
-        Stage stageAtual = (Stage) getBotaoSair().getScene().getWindow();
-        Navegacao.trocarTela(stageAtual, "/view/EditarPerfil.fxml", "Editar Perfil");
-    }
-    
     public void mostrarIdade() {
         labelIdade.setText(String.valueOf(paciente.getIdade()));
     }
     public void mostrarPlanoSaude() {
         labelPlanoSaude.setText(paciente.isTemPlano() ? "Sim" : "Não");
     }
-    @Override
-    public Button getBotaoSair() {
-        return buttonSair;
+    
+    @FXML
+    private void handleMarcarConsulta() {
+    try {
+       FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/TelaCadastrarConsulta.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Marcar Consulta");
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+    URL resource = getClass().getResource("/view/TelaCadastrarConsulta.fxml");
+    System.out.println("Caminho encontrado: " + resource);
 }
+
+}
+
