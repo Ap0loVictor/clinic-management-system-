d13e7eab808f9e0fb43fd42caa9060f166df797e
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2025-07-14 21:27:18 -0300

    Minhas consultas implementada, faltando botao de cancelar

--- clinic-management-system-/src/main/java/clinicamed/controller/MinhasConsultasController.java
+++ clinic-management-system-/src/main/java/clinicamed/controller/MinhasConsultasController.java
@@ -1,17 +1,26 @@
 package clinicamed.controller;
 
+import clinicamed.dao.ConsultaDao;
 import clinicamed.model.Consulta;
+import clinicamed.model.Paciente;
+import clinicamed.utils.Navegacao;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import javax.xml.stream.XMLInputFactory;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MinhasConsultasController extends Basecontroller implements Initializable {
-
+    private Paciente paciente;
     @FXML private Button voltar;
     @FXML private TableView<Consulta> consultas;
     @FXML private TableColumn<Consulta, String> nomeMedico;
@@ -26,6 +35,32 @@ public class MinhasConsultasController extends Basecontroller implements Initial
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        nomeMedico.setCellValueFactory(new PropertyValueFactory<>("nomeMedico"));
+        data.setCellValueFactory(new PropertyValueFactory<>("data"));
+        horario.setCellValueFactory(new PropertyValueFactory<>("horario"));
+        status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        descricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        if(paciente != null) {
+            List<Consulta> minhasConsultas = ConsultaDao.buscarPorPaciente(paciente.getNome());
+            consultas.getItems().setAll(minhasConsultas);
+        }
+    }
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+        List<Consulta> allConsultas = ConsultaDao.carregarConsultas();
+        List<Consulta> myConsultas = new ArrayList<>();
+        for (Consulta consulta : allConsultas) {
+            if(consulta.getNomePaciente().equalsIgnoreCase(paciente.getNome())) {
+                myConsultas.add(consulta);
+            }
+        }
+        consultas.getItems().setAll(myConsultas);
+    }
+    @FXML
+    public void handleVoltar() {
+        Stage atual = (Stage) voltar.getScene().getWindow();
+        Navegacao.trocarTela(atual, "/view/TelaPaciente.fxml", "Área do Paciente", controller -> {
+            ((PacienteController) controller).setPaciente(paciente);
+        });
     }
 }
--- clinic-management-system-/src/main/java/clinicamed/controller/PacienteController.java
+++ clinic-management-system-/src/main/java/clinicamed/controller/PacienteController.java
@@ -73,6 +73,13 @@ public class PacienteController implements Initializable {
             ((EditarController) controller).setUsuario(paciente);
         });
     }
+    @FXML
+    private void handleMinhasConsultas() {
+        Stage stageAtual = (Stage) buttonEditarPerfil.getScene().getWindow();
+        Navegacao.trocarTela(stageAtual, "/view/MinhasConsultas.fxml", "Minhas Consultas", controller -> {
+            ((MinhasConsultasController) controller).setPaciente(paciente);
+        });
+    }
 
     @FXML
 private void handleMarcarConsulta() {
