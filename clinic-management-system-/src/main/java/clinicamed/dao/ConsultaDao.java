ec66e00c810ae37d7b94cd24193db2c3c8c36735
unknown <davijannsen@gmail.com>
2025-07-15 16:20:18 -0300

    eliminando bug matador

--- clinic-management-system-/src/main/java/clinicamed/dao/ConsultaDao.java
+++ clinic-management-system-/src/main/java/clinicamed/dao/ConsultaDao.java
@@ -91,7 +91,7 @@ public class ConsultaDao {
                 if (dados.length < 6) continue;
                 
                 Consulta consulta = new Consulta(
-                    dados[0], dados[1], dados[2], dados[3], dados[4], dados[5]
+                    dados[1], dados[0], dados[2], dados[3], dados[4], dados[5]
                 );
                 
                 // Se tiver avaliação (campo 7)
